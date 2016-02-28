31a4b5a4873a01c60b59668a149f905235fb90e6
unknown <deborabpina@gmail.com>
2016-02-27 23:12:40 -0300

    Tela de game over quando o jogador toca o inimigo.

--- js/model/Enemy.js
+++ js/model/Enemy.js
@@ -38,7 +38,7 @@ Enemy.prototype =
     {
         this.chooseDirection();
 		this.seenPlayer();
-		//this.collidedWithPlayer();
+		this.collidedWithPlayer();
 		this.move();
     },
 	
@@ -128,8 +128,9 @@ Enemy.prototype =
 	collidedWithPlayer : function ()
 	{
 		if ((this.box.intersectsMesh(this.player.box, false))){
-			//GAME OVER aqui 
+			//the game ends when the player touchs the enemy
 			this.player.destroy();
+			window.location = 'gameover.html';
 		}
 	},
 	
